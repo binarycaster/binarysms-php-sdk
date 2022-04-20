d14a4c7cef6d26bd831f61619684a449c2ba050d
Akbar Hossain Robin <akbar.hossain@dh.health>
2022-04-20 23:42:07 +0600

    add `Binarysms` class with basic required properties and methods

--- src/Binarysms.php
+++ src/Binarysms.php
@@ -3,28 +3,73 @@
 namespace Binarycaster\Binarysms;
 
 
-class Binarysms
+class Binarysms extends ApiClient
 {
     public $config;
+    protected $msisdnPrefix = '880';
+    protected $msisdns = [];
+    protected $text;
+    protected $senderId;
+    protected $callbackUrl;
 
     public function __construct(Config $config)
     {
+        parent::__construct();
+
         $this->config = $config;
+        $this->senderId = $this->config->get('default-sender-id');
+        $this->setHeaders([
+            'App-Key' => $this->config->get('app-key'),
+            'App-Secret' => $this->config->get('app-secret'),
+        ]);
     }
 
-    /**
-     * @param $name
-     *
-     * @return  string
-     */
-    public function sayHello($name)
+    public function setSenderId($value)
     {
-        return $this->config->get('greeting') . ' ' . $name;
+        $this->senderId = $value;
+
+        return $this;
     }
 
-    public function sayBye($name)
+    public function setMsisdnPrefix($prefix)
     {
-        return $this->config->get('greeting') . ' ' . $name;
+        $this->msisdnPrefix = $prefix;
+
+        return $this;
     }
 
+    public function to($msisdn)
+    {
+        if (is_array($msisdn)) {
+            $this->msisdns = array_merge($this->msisdns, array_map(function ($item) {
+                return sprintf("{$this->msisdnPrefix}%s", substr($item, -10));
+            }, $msisdn));
+        } else {
+            $this->msisdns[] = $this->addMsisdnPrefix($msisdn);
+        }
+
+        return $this;
+    }
+
+    public function text($body)
+    {
+        $this->text = trim($body);
+
+        return $this;
+    }
+
+    public function send()
+    {
+        return $this->post($this->endpoint . 'v1/sms/send', [
+            'to' => $this->msisdns,
+            'text' => $this->text,
+            'sender_id' => $this->senderId,
+            'callback_url' => $this->callbackUrl,
+        ]);
+    }
+
+    private function addMsisdnPrefix($msisdn): string
+    {
+        return sprintf("{$this->msisdnPrefix}%s", substr($msisdn, -10));
+    }
 }
